87ae453358429f0b4194945fa328b8dc7fa3f896
Narpath Singh <narpathsingh208@gmail.com>
2025-02-08 19:00:14 +0530

    day 1

---
+++ Day1/app.py
@@ -0,0 +1,7 @@
+#band name generator
+
+print('Welcome to the band Name Generator')
+cityname = input("What's the name of the city you grew up in?\n")
+petname = input("What's your pet name?\n")
+
+print('Your band name could be ', cityname + ' ' + petname)
